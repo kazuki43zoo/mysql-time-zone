398c7dac254753302bb911558fd36a04b824419d
Kazuki Shimizu <kazuki43zoo@gmail.com>
2021-08-07 22:28:26 +0900

    Add insert test

--- mysql-jdbc-driver/src/test/java/com/example/mysqljdbcdriver/MysqlJdbcDriverApplicationWithTimeZoneParameterTests.java
+++ mysql-jdbc-driver/src/test/java/com/example/mysqljdbcdriver/MysqlJdbcDriverApplicationWithTimeZoneParameterTests.java
@@ -63,4 +63,30 @@ class MysqlJdbcDriverApplicationWithTimeZoneParameterTests {
     System.out.println(jdbcTemplate.queryForObject("SELECT item_datetime FROM DATE_TEST", Timestamp.class));
   }
 
+  @Test
+  void contextLoads2() throws SQLException {
+    jdbcTemplate.update("DELETE FROM DATE_TEST");
+    jdbcTemplate.update("INSERT INTO DATE_TEST VALUES(current_timestamp(3), current_timestamp(3))");
+    try (Connection c = DriverManager.getConnection(mysql.getJdbcUrl(), mysql.getUsername(), mysql.getPassword()); Statement s = c.createStatement()) {
+      ResultSet rs = s.executeQuery("SELECT * FROM DATE_TEST");
+      if (rs.next()) {
+        System.out.println(rs.getObject(1, ZonedDateTime.class));
+        System.out.println(rs.getObject(2, ZonedDateTime.class));
+      }
+      rs.close();
+    }
+    System.out.println(TimeZone.getDefault());
+    System.out.println("timestamp type:");
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_timestamp FROM DATE_TEST", LocalDateTime.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_timestamp FROM DATE_TEST", ZonedDateTime.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_timestamp FROM DATE_TEST", OffsetDateTime.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_timestamp FROM DATE_TEST", Date.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_timestamp FROM DATE_TEST", Timestamp.class));
+    System.out.println("datetime type:");
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_datetime FROM DATE_TEST", LocalDateTime.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_datetime FROM DATE_TEST", ZonedDateTime.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_datetime FROM DATE_TEST", OffsetDateTime.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_datetime FROM DATE_TEST", Date.class));
+    System.out.println(jdbcTemplate.queryForObject("SELECT item_datetime FROM DATE_TEST", Timestamp.class));
+  }
 }
